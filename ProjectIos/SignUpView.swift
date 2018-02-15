cc3cb21857fb1c27dccc83bd2658259842fb0c28
Matthieu Sukho <matthieu.sukho@u-psud.fr>
2018-02-15 12:21:47 +0100

    Squelette projet

---
+++ ProjectIos/SignUpView.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpView.swift
+//  ProjectIos
+//
+//  Created by Matthieu Sukho on 15/02/2018.
+//  Copyright © 2018 Matthieu Sukho. All rights reserved.
+//
+
+import UIKit
+
+class SignUpView: UIView {
+
+    @IBOutlet var SIgnUpView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        
+    }
+
+}
---
+++ ProjectIos/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  ProjectIos
+//
+//  Created by Matthieu Sukho on 15/02/2018.
+//  Copyright © 2018 Matthieu Sukho. All rights reserved.
+//
+
+import Foundation
+
+
+class User {
+    var email: String
+    var password: String
+    
+    init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+    
+}
